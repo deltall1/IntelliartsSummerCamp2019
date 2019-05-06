b86a36ca921b86e796c3d17ab49ed2271c89cfd4
Andrey <andrey.alekeev.1910@gmail.com>
2019-05-07 00:49:23 +0300

    Api routes

--- routes/api.js
+++ routes/api.js
@@ -2,10 +2,11 @@ const router = require("express").Router();
 const purchaseController = require("../controllers/purchase");
 const allController = require("../controllers/all");
 const clearController = require("../controllers/clear");
+const reportController = require("../controllers/report");
 
 router.post("/purchase", purchaseController.purchase);
 router.get("/all", allController.all);
 router.post("/clear", clearController.clear);
-// router.post("/report", func)
+router.post("/report", reportController.report);
 
 module.exports = router;
